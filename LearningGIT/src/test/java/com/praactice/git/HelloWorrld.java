77b880b1a7a2ffb0dd7ca3451c16f21a1eb72015
sabina <sabina@sabinas-mbp.lan>
2023-05-13 11:28:03 -0400

    Adding printing in console

--- LearningGIT/src/test/java/com/praactice/git/HelloWorrld.java
+++ LearningGIT/src/test/java/com/praactice/git/HelloWorrld.java
@@ -2,4 +2,11 @@ package com.praactice.git;
 
 public class HelloWorrld {
 
+	
+	public static void main(String[] args) {
+		// printing hw string
+		
+		System.out.println("Hello World! Get some Coffee");
+		
+	}
 }
